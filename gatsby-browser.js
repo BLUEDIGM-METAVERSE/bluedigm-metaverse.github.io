bbfad48da05be0a34f7d9b6563d72d9e85b64652
(주)비디 백승주 <sjpaek@bluedigm.com>
2023-04-03 14:26:44 +0900

    Initial Gatsby Setting

---
+++ gatsby-browser.js
@@ -0,0 +1,2 @@
+// https://github.com/PrismJS/prism/tree/1d5047df37aacc900f8270b1c6215028f6988eb1/themes
+import 'prismjs/themes/prism-tomorrow.css';
---
+++ gatsby-node.js
@@ -0,0 +1,33 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
+ */
+
+const path = require('path');
+const { createFilePath } = require(`gatsby-source-filesystem`);
+
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {};
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, 'src/components'),
+        utils: path.resolve(__dirname, 'src/utils'),
+        hooks: path.resolve(__dirname, 'src/hooks'),
+      },
+    },
+  });
+};
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode });
+
+    createNodeField({ node, name: 'slug', value: slug });
+  }
+};
